43f71ca270e56164ece4100159b4ca5e202c792c
Codica Studio <CodicaStudioDev@gmail.com>
2020-09-04 14:25:48 -0500

    Test todo

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// TODO Test One from Routes
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
+ */
 
 Route::get('/', function () {
     return view('welcome');
